0594ca42f2dda80f0a719caa7caf8ad100e1eb38
Abizer-R <rabizeridris@gmail.com>
2022-03-23 21:12:46 +0530

    Made RegisterActivity

---
+++ app/src/main/java/com/example/firebaseexample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.firebaseexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private Button registerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.login_btn);
+        registerBtn = findViewById(R.id.register_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firebaseexample/RegisterActivity.java
@@ -0,0 +1,69 @@
+package com.example.firebaseexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button registerBtn;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.register_activity_email);
+        password = findViewById(R.id.register_activity_password);
+        registerBtn = findViewById(R.id.register_activity_register_button);
+
+        auth = FirebaseAuth.getInstance();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                String emailEntered = email.getText().toString();
+                String passwordEntered = password.getText().toString();
+                
+                if(TextUtils.isEmpty(emailEntered) || TextUtils.isEmpty(passwordEntered)) {
+                    Toast.makeText(RegisterActivity.this, "Please don't leave the fields empty", Toast.LENGTH_SHORT).show();
+                } else if(passwordEntered.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password should consist at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(emailEntered, passwordEntered);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String emailEntered, String passwordEntered) {
+        auth.createUserWithEmailAndPassword(emailEntered, passwordEntered)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Registration failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
